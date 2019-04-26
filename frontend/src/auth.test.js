0ec91b5daff88b18ea014d97c018dec2320029b9
vengelmann <vengelmann@localhost.localdomain>
2019-04-26 06:12:32 -0700

    commenting out axiosMock token header test

--- frontend/src/auth.test.js
+++ frontend/src/auth.test.js
@@ -1,7 +1,6 @@
 import { googleStoredTokenId, setAuthHeaderOnAxios } from './auth'
 import constants from './constants';
 import 'jest-localstorage-mock';
-import mockAxios from 'jest-mock-axios';
 
 
 test("time-namespace-localStorage", () => {
@@ -11,13 +10,3 @@ test("time-namespace-localStorage", () => {
   const storedLoc = googleStoredTokenId();
   expect(storedLoc).toEqual(VALUE);
 });
-
-test("auth-header-added", () => {
-    const axios = setAuthHeaderOnAxios(mockAxios);
-    // requesting data from server
-    let _ = axios.post('/whatever/', { data: "hello" });
- 
-    // converting server response to upper case
-    let lastReqInfo = axios.lastReqGet();
-    console.log(lastReqInfo.headers);
-})
