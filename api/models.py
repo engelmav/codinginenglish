d3661f96040c8928dced9a28cc5de4b1f2b45c9a
vengelmann <vengelmann@localhost.localdomain>
2019-04-27 15:40:16 -0700

    adding secrets.json

--- api/models.py
+++ api/models.py
@@ -1,16 +1,18 @@
 from peewee import *
 
 
-cie_db = SqliteDatabase('coding_in_english.db')
+db = MySQLDatabase("cie",
+                       host="localhost",
+                       port=3306,
+                       user="appuser",
+                       password="appuser")
 
 
 class User(Model):
     first_name = CharField()
     last_name = CharField()
     google_sub_id = BigIntegerField(default=0)
-    created_date = DateField()
+    created_date = DateTimeField()
 
     class Meta:
-        database = cie_db
-
-cie_db.create_tables([User])
+        database = db
