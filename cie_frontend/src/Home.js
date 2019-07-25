bb6380a6549eae79c7eac7e4e7261ba49950cda8
vengelmann <vengelmann@localhost.localdomain>
2019-07-25 06:56:52 -0700

    Home component greets user with first name

--- cie_frontend/src/Home.js
+++ cie_frontend/src/Home.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+  // this.state.authData
   render() {
     const { isAuthenticated } = this.props.auth;
+    let userFirstName = null;
+    if (this.props.authData !== null){
+      userFirstName = this.props.authData.idTokenPayload.given_name;
+    }
     return (
       <div>
         Home
         {
           isAuthenticated() && (
               <h4>
-                You are logged in!
+                Welcome, {userFirstName}!
               </h4>
             )
         }
