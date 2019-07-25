f49e89305664cc732e7994930ce399bf87b961b3
vengelmann <vengelmann@localhost.localdomain>
2019-07-25 06:58:40 -0700

    zoom component uses param passed to iframe for zoom username

--- zoom_frontend/js/index.js
+++ zoom_frontend/js/index.js
@@ -9,11 +9,33 @@ console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
 
+function getUserName() {
+  const params = window.location.href.split('?')[1].split('&');
+  const data = {};
+
+  params.forEach((param) => {
+    const splitParam = param.split('=');
+    const key = splitParam[0];
+    const value = splitParam[1];
+    data[key] = value;
+  });
+  if ('userName' in data) {
+    return data.userName;
+  }
+  return null;
+}
+
+
 function joinMeeting(meetingNumber) {
+  let userName = getUserName();
+  if (userName === null) {
+    userName = document.getElementById('display_name').value;
+  }
+  console.log('joinMeeting() got userName', userName);
+
   axios.get(`/zoom/signature/${meetingNumber}/${Date.now()}`)
     .then((res) => {
       const { signature, apiKey } = res.data;
-      const userName = 'Vincenz'; // document.getElementById('display_name').value;
       console.log(`Joining user ${userName} to zoom ID ${meetingNumber}`);
       ZoomMtg.init({
         leaveUrl: 'http://127.0.0.1:5002/',
@@ -55,7 +77,6 @@ const addZoom = () => {
 
 class ZoomApp extends Component {
   componentDidMount() {
-    console.log('ZoomApp component mounted');
     addZoom();
     const source = new EventSource('/stream');
     source.onmessage = (event) => {
