5c4f597b9de0a34a46bc6e0128cdbf20d4294edb
vengelmann <vengelmann@localhost.localdomain>
2019-07-25 06:56:31 -0700

    root App component now captures auth details and passes down to children components

--- cie_frontend/src/App.js
+++ cie_frontend/src/App.js
@@ -26,21 +26,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      authData: null
     };
   }
 
   async componentDidMount() {
     if (window.location.pathname.includes('/callback')) return;
     try {
-      await auth.silentAuth();
+      await auth.silentAuth(this.setIsAuthenticated);
       this.forceUpdate();
     } catch (err) {
       if (err.error !== 'login_required') console.log(err.error);
     }
   }
 
-  setIsAuthenticated = () => this.setState({ isAuthenticated: true })
+  setIsAuthenticated = (authData) => {
+    console.log("authData from setIsAuthenticated:", authData);
+    this.setState(
+      {
+        isAuthenticated: true,
+        authData
+      }
+    );
+  }
 
   render() {
     return (
@@ -56,8 +65,8 @@ class App extends Component {
             <li><Login auth={auth} isAuthenticated={auth.isAuthenticated()} /></li>
           </ul>
           <>
-            <Route exact path="/home" component={(props) => <Home auth={auth} {...props} />} />
-            <Route exact path="/class" component={(props) => <ClassRoomProtected {...props} />} />
+            <Route exact path="/home" component={(props) => <Home auth={auth} authData={this.state.authData} {...props} />} />
+            <Route exact path="/class" component={(props) => <ClassRoomProtected authData={this.state.authData} {...props} />} />
             <Route path="/callback" render={(props) => {
               handleAuthentication(props, this.setIsAuthenticated);
               return <CallbackWithRouter {...props} />
