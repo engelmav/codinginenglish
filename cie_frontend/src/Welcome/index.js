eaa63d05257dd6b08e1c0a838d9fd903c25d6d55
vengelmann <vengelmann@localhost.localdomain>
2019-08-26 09:33:57 -0700

    colorized cards

--- cie_frontend/src/Welcome/index.js
+++ cie_frontend/src/Welcome/index.js
@@ -61,7 +61,7 @@ class ModuleCard extends Component {
     return (
       <div className="module-card">
         <img src={cie_module.image_path} />
-        <p>{cie_module.name}</p>
+        <h1>{cie_module.name}</h1>
         <p>{localDateTime}</p>
         <button onClick={this.onClick}>SIGN UP</button>
       </div>
