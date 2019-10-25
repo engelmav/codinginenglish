f46132bfef10fec5bf00777840752a2ae35f6bb2
Vincent Engelmann <vengelmann@ntent.com>
2019-10-24 20:27:41 -0700

    removed extraneous import script

--- cie_api/database/import.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "*** Importing CIE schema"
-mysql -u root -p$MYSQL_ROOT_PASSWORD < /tmp/schema.sql
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on *.* to 'appuser'@'%';"
