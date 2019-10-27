d3037b2a8ef991f3d475af72df5e76c1c3173e6e
Vincent Engelmann <vengelmann@ntent.com>
2019-10-26 20:18:05 -0700

    db session resiliency

--- cie_api/database/database.py
+++ cie_api/database/database.py
@@ -1,13 +1,48 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import QueuePool, Pool
+from sqlalchemy import exc, event
+import pymysql
 
 from config import config
 
+def get_conn():
+    cnx = pymysql.connect(
+        user="appuser",
+        password=db_password,
+        host="cie-db",
+        database="cie")
+    return cnx
+
+pool = QueuePool(
+    creator=get_conn,
+    pool_size=36,      # workercount * pool_size must be < MySQL max_connections
+    max_overflow=0,    # allows you to go higher than pool_size allowed
+    timeout=20,        # seconds before giving up acquiring a connection from pool
+    recycle=299)       # refresh the pool ever 299 seconds
 
 db_password = config["cie.database.password"]
-engine = create_engine(f'mysql+pymysql://appuser:{db_password}@cie-db/cie')
+engine = create_engine('mysql+pymysql://', pool=pool)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+
+@event.listens_for(Pool, "checkout")
+def on_conn_checkout(dbapi_con):
+    """
+    This is a manual version of pessimistic connection ping. Necessary for mysql.
+    http://docs.sqlalchemy.org/en/rel_0_8/core/pooling.html#disconnect-handling-pessimistic
+    """
+    cursor = dbapi_con.cursor(buffered=True)
+    try:
+        cursor.execute("SELECT 1") # dbapi_con.ping() equivalent
+    except exc.OperationalError as ex:
+        if ex.args[0] in (2006,   # MySQL server has gone away
+                          2013,   # Lost connection to MySQL server during query
+                          2055):  # Lost connection to MySQL server at '%s', system error: %d
+            raise exc.DisconnectionError()
+        else:
+            raise
 
 
 class CustomBase(object):
