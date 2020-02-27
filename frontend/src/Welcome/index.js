aabd33d5f3f580c80e3ae849bb89f7cff704bc83
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-02-26 19:09:19 -0800

    add Stripe skeleton elements

--- frontend/src/Welcome/index.js
+++ frontend/src/Welcome/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { DateTime } from 'luxon';
+import Modal from 'react-modal';
+import { Elements, CardElement } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
 import './styles.css';
 
 class CieApi {
@@ -24,7 +28,7 @@ class CieApi {
 
 var cieApi = new CieApi();
 
-export default class Welcome extends Component {
+class Welcome extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,19 +64,64 @@ export default class Welcome extends Component {
 
 
 class ModuleCard extends Component {
-  onClick = () => cieApi.startSignup(this.props.sessionData.id, 1);
+  constructor(){
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+    this.stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+
+  }
+  handleSignupClick = () => {
+    this.setState({modalIsOpen: true});
+    cieApi.startSignup(this.props.sessionData.id, 1);
+  }
+  afterOpenModal= () => {
+    // ?
+  }
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
   render() {
     let { cie_module, session_datetime } = this.props.sessionData;
     console.log("UTC from server:", session_datetime);
     var local = DateTime.fromISO(session_datetime);
     const localDateTime = local.toLocaleString(DateTime.DATETIME_FULL);
+    const { modalIsOpen } = this.state;
+    const { afterOpenModal, closeModal } = this;
     return (
       <div className="module-card">
         <img src={cie_module.image_path} />
         <h1>{cie_module.name}</h1>
-        <p className="datetime">{localDateTime}</p>
-        <button onClick={this.onClick}>SIGN UP</button>
+        <p className="datetime">Starts {localDateTime}</p>
+        <button onClick={this.handleSignupClick}>SIGN UP</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}>
+            <h1>Do they speak English in What?</h1>
+            <Elements stripe={this.stripePromise}>
+              <CardElement
+                options={{
+                  style: {
+                    base: {
+                      fontSize: '16px',
+                      color: '#424770',
+                      '::placeholder': {
+                        color: '#aab7c4',
+                      },
+                    },
+                    invalid : {
+                      color: '#9e2146',
+                    }
+                  }
+                }}
+              />
+            </Elements>
+        </Modal>
       </div>
     );
   }
 }
+
+export { Welcome, ModuleCard };
