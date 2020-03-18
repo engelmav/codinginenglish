c56ad8ecd0d1b673eac8672d39e3994601d6a012
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-03-18 06:28:39 -0700

    Alert component takes children

--- frontend/src/UtilComponents/AlertMessage/index.js
+++ frontend/src/UtilComponents/AlertMessage/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './style.module.css';
 
 const AlertMessage = (props) => {
+    const { style, text } = props;
     return (
-        <div className={styles.border}>{props.text}</div>
-    )
+        <div className={styles.border} style={style}>{text}</div>
+    );
 }
 
 export { AlertMessage };
