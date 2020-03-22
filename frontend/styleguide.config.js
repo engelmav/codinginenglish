8c26af6be745a7e7b9da74fd045a61bc0ffff41c
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-03-22 07:42:01 -0700

    added google font injection o styleguidist

--- frontend/styleguide.config.js
+++ frontend/styleguide.config.js
@@ -1,11 +1,21 @@
 module.exports = {
     ignore: ['src/history.js',
-    'src/index.js',
-    'src/**/*.test.js',
-    'src/**/*.config.*',
-    'src/jsconfig.js',
-    'src/serviceWorker.js'
+        'src/index.js',
+        'src/**/*.test.js',
+        'src/**/*.config.*',
+        'src/jsconfig.js',
+        'src/serviceWorker.js'
     ],
     components: 'src/**/*.js',
+    template: {
+        head: {
+            links: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css?family=Roboto'
+                }
+            ]
+        }
+    },
     webpackConfig: require('./config/webpack.config.js')
 }
