1b0e6853c1933acda4f50f2a3598e093873b4c30
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-03-22 07:36:47 -0700

    add base button styles

--- frontend/src/UtilComponents/Button/index.js
+++ frontend/src/UtilComponents/Button/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import styles from './styles.css';
+// import styles from './styles.css';
+import styled from 'styled-components';
+import { color, space } from 'styled-system';
 
 
-const Button = (props) => {
-    const {
-        btnType,
-        children
-    } = props;
-    return (
-        <button className={btnType}>{children}</button>
-    );
-};
+const Button = styled.div`
+  font-family: 'Roboto', serif;
+  background-color: white;
+  border: solid #ff3e00;
+  border-width: 2px;
+  color: #ff3e00;
+  padding: 8px;
+  border-radius: 7px;
+  &:hover {
+    color: white;
+    background-color: #ff3e00;
+  }
+  ${space}
+`
 
 export { Button };
