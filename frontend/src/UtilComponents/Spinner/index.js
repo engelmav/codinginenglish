d9379f78061ddc4c0ffe2ab815f20799197a45ed
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-03-31 20:43:14 -0700

    add Spinner

---
+++ frontend/src/UtilComponents/Spinner/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PulseLoader from 'react-spinners/PulseLoader';
+
+
+const Spinner = () =>
+        <PulseLoader loading={true} />
+
+
+/** @component */
+export { Spinner };
