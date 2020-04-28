659ec817f03fdb5d38350c15b6a8e5298257368e
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-04-27 20:09:12 -0700

    add payment logger

--- api/payment_endpoints.py
+++ api/payment_endpoints.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
 import stripe
+import logging
 
 from config import config
 
-
+LOG = logging.getLogger(__name__)
 stripe_bp = Blueprint('stripe_payments', __name__)
 stripe_secret = config.get('cie.api.stripe.secretkey')
 stripe_publishable_key = config.get('cie.api.stripe.publishablekey')
@@ -13,12 +14,13 @@ stripe.api_key = stripe_secret
 
 def calc_order_amount(item):
     # TODO: pull from database
-    return 50
+    return 1
 
 
 @stripe_bp.route('/payment/create-payment-intent', methods=['POST'])
 def create_payment():
     data = request.get_json()
+    LOG.info("Creating payment: %s" % str(data))
     intent = stripe.PaymentIntent.create(
         # TODO: turn this into a lookup. CIE classes may have different costs
         amount=calc_order_amount(data.get('item')),
