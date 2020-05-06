65e5d3bb5b7e741ef2ff1f6c92705b7e621f25fc
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-05-05 20:35:14 -0700

    fix lint issue

--- frontend/src/UtilComponents/TextInput/index.js
+++ frontend/src/UtilComponents/TextInput/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 
 
-const TextInput = styled.input.attrs(props => ({
-  type: 'text'
-}))
-`
+const TextInput = styled.input.attrs(props => ({ type: 'text' }))`
   border-radius: 3px;
-`
+`;
+
 export { TextInput };
