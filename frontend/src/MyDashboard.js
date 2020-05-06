a68f24d74a04c413fea29eb29e94005b1edf517b
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-05-05 20:34:32 -0700

    add prelim user sessions in my dashboard. add styled components for semantic html.

--- frontend/src/MyDashboard.js
+++ frontend/src/MyDashboard.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { observer } from 'mobx-react';
+import styled from 'styled-components';
+import { font } from './UtilComponents/sharedStyles';
+import { Title, Main } from './UtilComponents';
 
-export default class MyDashboard extends Component {
-  // this.state.authData
-  render() {
-    const { isAuthenticated } = this.props.auth;
-    let userFirstName = null;
-    if (this.props.authData !== null){
-      userFirstName = this.props.authData.idTokenPayload.given_name;
-    }
-    return (
-      <div>
-        Home
+
+const ContentWrapper = styled.div`
+  ${font}
+  margin: 0 auto;
+  max-width: 980px;
+  min-width: 769px;
+`;
+
+
+const MyDashboard = observer(props => {
+
+  const { appStore } = props;
+
+  return (
+    <Main>
+      <Title>{`${appStore.firstName}'s Dashboard`}</Title>
+      <ContentWrapper style={{ margin: "0 auto", maxWidth: "980px", minWidth: "769px" }}>
+        
         {
-          isAuthenticated() && (
-              <h4>
-                Welcome, {userFirstName}!
-              </h4>
-            )
+          appStore.authData && (
+            <h4>
+              Welcome, {appStore.firstName}!
+            </h4>
+          )
         }
         <p>Your class is in session. Go there now!</p>
+        <p>Currently registered classes:</p>
+        {appStore.userSessions &&
+          <ul>
+            {appStore.userSessions.map(sess =>
+              <li>{sess.module_session_id}</li>
+            )}
+          </ul>
+        }
         <p>Your next classes</p>
         <p>Find More Classes</p>
         <p>Words for me to learn</p>
-      </div>
-    )
-  }
-}
+      </ContentWrapper>
+    </Main>
+  );
+
+});
+
+export default MyDashboard;
