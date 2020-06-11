4d1c059751a9cee13f31023ad23eaedefac78ffb
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-06-10 21:51:57 -0700

    remove unneeded import

--- frontend/src/UtilComponents/Title/index.js
+++ frontend/src/UtilComponents/Title/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { typography, space } from 'styled-system';
-import { font, fontColor } from '../sharedStyles';
+import {  fontColor } from '../sharedStyles';
 
 
 const Title = styled.h1`
