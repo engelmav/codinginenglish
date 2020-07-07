2e5c7fff2b406647f60b588bb65ed83c02e1ce44
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-07-07 07:09:32 -0700

    move supporting components out of module, add computedEmail for loginless payment

--- web/src/CheckoutForm/index.js
+++ web/src/CheckoutForm/index.js
@@ -4,85 +4,34 @@ import {
 import { loadStripe } from '@stripe/stripe-js';
 import React, { useState } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import settings from '../settings';
 
 import {
   AlertMessage,
   Button,
   Spinner,
-  TextInput,
 } from '../UtilComponents';
 
-import { font } from '../UtilComponents/sharedStyles';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import 'mobx-react-lite/batchingForReactDom';
+import { CcySelect, EmailNote, Form, PaymentInfo, PmtFormLabel, NameField, BuyButton } from './subcomponents';
+
+const { stripePK } = settings;
+
+let paymentCurrency = observable.box('EUR');
+const setPaymentCurrency = (e) => paymentCurrency = e.target.value;
 
 
-/* global window */
-window.enablePayments = true;
-
-const stripePromise = loadStripe('pk_test_cwKTnilflzQHY5WlR2x2tgwa00KGJyLRrP');
-
-const PaymentInfo = styled.div`
-  ${font}
-  display: grid;
-  grid-template-columns: min-content auto;
-  grid-gap: 0.6em;
-  align-items: center;
-  grid-auto-flow: dense;
-`;
-const NameField = styled(TextInput)`
-  grid-column: 2 / 3;
-  width: auto;
-  margin: 0;
-  border: none;
-  outline: none;
-`;
-const PmtFormLabel = styled.label`
-  align-self: start;
-  text-align: right;
-  width: auto;
-  padding: 0;
-  padding-top: 1px;
-  margin: 0;
-  font-size: 85%;
-  font-weight: 600;
-  white-space: nowrap;
-
-`;
-const BuyButton = styled(Button)`
-  grid-column: 2 / 3;
-  border-color: black;
-  background-color: black;
-  color: white;
-  &:hover {
-    color: black;
-    background-color: white;
-  }
-  margin-top: 6px;
-  justify-self: start;
-`;
-
-const CcySelect = styled.select`
-  justify-self: start;
-`;
+const stripePromise = loadStripe(stripePK);
 
 const CurrencyMenu = () => {
   return (
-    <CcySelect><option>EUR</option><option>USD</option></CcySelect>
+    <CcySelect value={paymentCurrency} onChange={setPaymentCurrency}>
+      {['EUR', 'USD'].map((currency, idx) => <option key={idx} value={currency}>{currency}</option>)}
+    </CcySelect>
   )
 };
-
-
-const Form = styled.form`
-
-`;
-
-const EmailNote = styled.p`
-  font-style: italic;
-  font-size: .8em;
-  margin: 0;
-  padding-top: 1px;
-  padding-bottom: 1px;
-`;
 
 
 function CheckoutFormConsumer(props) {
@@ -91,11 +40,12 @@ function CheckoutFormConsumer(props) {
   const elements = useElements();
   const [name, setName] = useState('');
   // const [postalCode, setPostalCode] = useState('');
-  const [email, setEmail] = useState('');
-  const [currency, setCurrency] = useState('EUR');
+  const [email, setEmail] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [isComplete, setComplete] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
+
+  const computedEmail = email || appStore.email; // prioritize a manually entered email over the email we find in the auth user object.
 
   const handleSubmit = async (event) => {
     // disable default form submission
@@ -116,27 +66,29 @@ function CheckoutFormConsumer(props) {
       }
     };
 
-
-
     const intentParams = {
       item: sessionData.id,
-      currency: currency
+      currency: paymentCurrency,
+      email: email
     };
-
 
     let clientSecret;
     try {
       setLoading(true);
-      const resp = await axios.post('/payment/create-payment-intent', intentParams);
+      const resp = await axios.post('/api/payment/create-payment-intent', intentParams);
       clientSecret = resp.data.clientSecret;
     } catch (error) {
       setErrorMsg("We couldn't process your order. You have not been charged. We are looking into the issue right now.");
       setLoading(false);
       console.log(error);
-      // TODO: capture failure at backend
+      try {
+        const failReason = { ...error, email: email };
+        const resp = await axios.post('/api/payment/failure', failReason);
+      } catch (err2) {
+        console.log("Failed to capture fail reason. Epic!", err2)
+      }
       return;
     }
-
 
     const result = await stripe.confirmCardPayment(clientSecret, paymentMethod);
 
@@ -148,7 +100,7 @@ function CheckoutFormConsumer(props) {
       // The payment was processed.
       console.log(result);
       if (result.paymentIntent.status === 'succeeded') {
-        // TOOD: turn into modal.
+        // TODO: turn into modal.
         setComplete(true);
         setLoading(false);
       }
@@ -164,19 +116,22 @@ function CheckoutFormConsumer(props) {
         :
         <Form onSubmit={handleSubmit}>
           <PaymentInfo className="FormGroup">
-            <PmtFormLabel>Name</PmtFormLabel><NameField placeholder="Marcus Aurelius" value={name} onChange={e => setName(e.target.value)} />
+            <PmtFormLabel>Name</PmtFormLabel>
+            <NameField placeholder="You Name Here" value={name} onChange={e => setName(e.target.value)} />
             <PmtFormLabel>Card Details</PmtFormLabel><CardElement />
             {/* <PmtFormLabel>Postal/Zip Code</PmtFormLabel><NameField placeholder="08000" value={postalCode} onChange={e => setPostalCode(e.target.value)} /> */}
-            <PmtFormLabel>Currency</PmtFormLabel><CurrencyMenu value={currency} onChange={e => setCurrency(e.target.value)} />
-            {!appStore.authData &&
-              <>
-                <PmtFormLabel>Email</PmtFormLabel>
-                <div>
-                  <NameField placeholder="marcus.aurelius@gmail.com" value={email} onChange={e => setEmail(e.target.value)} />
-                  <EmailNote>For receipt and account creation on your terms. We will NOT spam you.</EmailNote>
-                </div>
-              </>
-            }
+            <PmtFormLabel>Currency</PmtFormLabel>
+            <CurrencyMenu />
+
+            <PmtFormLabel>Email</PmtFormLabel>
+            <div>
+              <NameField placeholder="some.name@domain.com"
+                value={computedEmail || ''} // need this empty string for React to "control" this input field
+                onChange={e => setEmail(e.target.value)}
+              />
+              <EmailNote>For receipt and account creation on your terms. We will NOT spam you.</EmailNote>
+            </div>
+
             <BuyButton onClick={handleSubmit} disabled={!stripe || isLoading}>
               PURCHASE
             </BuyButton>
@@ -190,14 +145,13 @@ function CheckoutFormConsumer(props) {
 
 
 function CheckoutForm(props) {
-
   return (
     <Elements stripe={stripePromise}>
-      {window.enablePayments ? <CheckoutFormConsumer appStore={props.appStore} onCloseClick={props.onCloseClick} sessionData={props.sessionData}  />
-        : <div>payments disabled</div>}
+      <CheckoutFormConsumer appStore={props.appStore} onCloseClick={props.onCloseClick} sessionData={props.sessionData} />
     </Elements>
   )
 }
 
+const CheckoutFormObserver = observer(CheckoutForm);
 
-export { CheckoutForm };
+export { CheckoutFormObserver as CheckoutForm };
