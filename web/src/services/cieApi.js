a1f32f284ff69ac1c88f5adce3a9e42b7be77b38
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-08-05 21:22:58 -0700

    add sendPaymentConfirmation

--- web/src/services/cieApi.js
+++ web/src/services/cieApi.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
 
-
 class CieApi {
   async storeNewUser(data) {
-    return await axios.post('/api/users', data);
+    return await (await axios.post('/api/users', data)).data;
   }
-  async userRegistrations(userId) {
+  async getUserRegistrations(userId) {
     return await axios.get(`/api/users/${userId}/module-sessions`);
   }
   async registerUserToSession(userId){
     return await axios.post(`/api/users/${userId}/module-sessions`);
+  }
+
+  async sendPaymentConfirmation(params){
+    return (await axios.put('/api/payment/confirmation', params)).data;
   }
 }
 
