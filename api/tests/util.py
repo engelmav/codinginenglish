11f7f796c2d71bcb238756963da5bacb65959c66
Vincent Engelmann <vincent.engelmann1@gmail.com>
2020-10-27 07:50:19 -0400

    use jsonapi style Response in object creation

--- api/tests/util.py
+++ api/tests/util.py
@@ -3,8 +3,7 @@ import datetime
 import requests
 
 
-cie_api = "0.0.0.0:5000"
-url = f"http://{cie_api}/api/cie-modules/19/sessions"
+cie_api = "http://0.0.0.0:5000/api"
 
 
 def session_start_date(start_date=None):
@@ -13,14 +12,28 @@ def session_start_date(start_date=None):
     return {"session_datetime": start_date}
 
 
+def create_test_user():
+    pass
+
+
 def create_2_hour_session():
-    resp = requests.post(url, params={"cie_module_id": 19}, json=session_start_date(),)
+    _url = f"{cie_api}/cie-modules/19/sessions"
+    resp = requests.post(_url, params={"cie_module_id": 19}, json=session_start_date(),)
+    print(resp)
+    print(resp.json())
+
+
+def register_user_to_session():
+    user_id = 10
+    payload = {"module_session_id": 17}
+    _url = f"f{cie_api}/users/{user_id}/module-sessions"
+    resp = requests.post(_url, json=payload)
     print(resp)
     print(resp.json())
 
 
 def delete_session(session_id):
-    _url = f"http://{cie_api}/api/module_sessions/{session_id}"
+    _url = f"{cie_api}/module_sessions/{session_id}"
     resp = requests.delete(_url)
     print(resp.json())
 
