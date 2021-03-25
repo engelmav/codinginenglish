6373beacef0ae75b57623b60e8cd6608728fcbb4
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-03-25 13:07:03 -0400

    tweak tagline, margins

--- web/src/Home/index.js
+++ web/src/Home/index.js
@@ -1,29 +1,21 @@
 import React from "react";
 import { Main, Title, ContentSection } from "../UtilComponents";
-import { lightGray, orangeBgColor, darkGray, debugBorder } from "../UtilComponents/sharedStyles";
+import {
+  lightGray,
+  orangeBgColor,
+  darkGray,
+  debugBorder,
+  cieOrange,
+} from "../UtilComponents/sharedStyles";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
-
+import { space, color, layout } from "styled-system";
 
 const MainLanding = styled(Main)`
   width: min(90%, 700px);
-  & h1 {
-    // font-size: 3vw;              // ok on desktop, too small on mobile
-    // font-size: 2.5rem;           // too big on mobile.
-    // font-size: min(2.5rem, 3vw); // ok on desktop, too small on mobile
-    // font-size: max(2.5rem, 3vw); // ok on desktop, too big on mobile
-    // font-size: 1.75rem; // ok on mobile, too small on desktop
-    font-size: max(1.75rem, min(2.5rem, 3vw));
-    font-family: sans-serif;
-    text-align: center;
-    color: ${darkGray}
-    ${debugBorder}
-  }
   & .cie-title {
     font-family: monospace;
     font-size: max(1rem, min(2.5rem, 3vw));
-    padding: 0;
-    margin: 0;
   }
   display: flex;
   flex-direction: column;
@@ -36,12 +28,13 @@ const MainLanding = styled(Main)`
 const ContentSectionLanding = styled(ContentSection)`
   padding-bottom: 20px;
   line-height: 1.5em;
+  margin-bottom: 1.5rem;
 `;
 
 const BlockQuote = styled.blockquote`
   ${lightGray}
   font-family: serif;
-  font-size: 1.5em;
+  font-size: 1.5rem;
 
   p {
     background: #f9f9f9;
@@ -81,14 +74,12 @@ const ImgContainer = styled.div`
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
-  margin-bottom: 30px;
 `;
 
 const RegisterLink = styled(Link)`
   ${orangeBgColor}
   text-decoration: none;
   display: inline-block;
-  width: 
   text-align: center;
   padding: 10px;
   color: white;
@@ -103,11 +94,119 @@ const RegisterButton = () => (
   </div>
 );
 
+const TaglineTitle = styled.h1`
+  // background-color: ${darkGray}
+  // color: white;
+  font-family: sans-serif;
+  color: ${darkGray};
+  text-transform: uppercase;
+  font-size: max(1rem, min(2.5rem, 2.5vw));
+  font-weight: bolder;
+  text-align: center;
+  padding:0;
+  margin:0;
+`;
+
+const TaglineSubtitle = styled.h2`
+  padding: 0;
+  margin: 0;
+  font-family: sans-serif;
+  text-transform: uppercase;
+  text-align: center;
+  font-size: max(0.75rem, min(1.5rem, 1.5vw));
+`;
+
+const SectionTitle = styled(Title)`
+  // font-size: 3vw;              // ok on desktop, too small on mobile
+  // font-size: 2.5rem;           // too big on mobile.
+  // font-size: min(2.5rem, 3vw); // ok on desktop, too small on mobile
+  // font-size: max(2.5rem, 3vw); // ok on desktop, too big on mobile
+  // font-size: 1.75rem; // ok on mobile, too small on desktop
+  font-size: max(1.75rem, min(2.5rem, 3vw));
+  font-family: sans-serif;
+  text-align: center;
+  color: ${darkGray};
+  ${debugBorder}
+`;
+
+const Box = styled.div(
+  {
+    boxSizing: "border-box",
+    minWidth: 0,
+  },
+  space,
+  color,
+  layout
+);
+
 const Home = (props) => {
   return (
     <MainLanding>
-      {/* <h1 className="cie-title">coding_in_english</h1> */}
-      <ContentSection className="section">
+      <Box mb={20}>
+        <Box mb={10}>
+          <TaglineTitle>Jump into the global mainstream.</TaglineTitle>
+        </Box>
+        <TaglineSubtitle>Learn English as you learn to code.</TaglineSubtitle>
+      </Box>
+      <SectionTitle>Speak English, Build Apps</SectionTitle>
+      <ContentSectionLanding>
+        <p>
+          Practice conversation in every class, specifically on software
+          engineering.
+        </p>
+        <p>
+          Topics range from working with product managers, co-workers, clients,
+          performing code reviews, asking questions on forums, reading
+          documentation, and many more.
+        </p>
+        <I>And build real software in the process.</I>
+        <RegisterButton />
+      </ContentSectionLanding>
+      <SectionTitle>Create Your Technical Portfolio</SectionTitle>
+      <ContentSectionLanding>
+        <p>Build your portfolio as you go.</p>
+        <p>
+          All your work in classes is yours, for display in your portfolio
+          WebApp. Show-off to potential employers your skills in ReactJS,
+          Python, and MySQL, with professional code in a GitHub repository, and
+          a real, deployed WebApp.
+        </p>
+        <Box p={20}>
+          <ImgContainer>
+            {[
+              "react-logo-150.png",
+              "python-logo-150.png",
+              "mysql-logo-150.png",
+            ].map((src, idx) => (
+              <img
+                alt={src}
+                key={idx}
+                style={{ height: "150px" }}
+                src={`${props.settings.assets}/${src}`}
+              />
+            ))}
+          </ImgContainer>
+        </Box>
+        <RegisterButton />
+      </ContentSectionLanding>
+      <SectionTitle>Learn Vocabulary and Grammar in Real Context</SectionTitle>
+      <ContentSectionLanding>
+        <p>
+          We supplement all courses with grammar and vocabulary practices,
+          tightly integrated into technical lessons.
+        </p>
+        <RegisterButton />
+      </ContentSectionLanding>
+      <SectionTitle>Learn in a Live Teaching Environment</SectionTitle>
+      <ContentSectionLanding>
+        <p>
+          With so many classes moving to massive open online courses, students
+          are left to learn in isolation. We bring back the warmth, motivation,
+          and community that online classes are currently lacking. Do pair work
+          activities in class, ask your instructor questions, laugh, and share
+          your doubts and your accomplishments!
+        </p>
+        <RegisterButton />
         <BlockQuote cite="https://medium.com/@lnuk2009jp/is-english-language-really-that-important-in-learning-programming-812a78be79b5">
           <p>
             "As a foreign student, learning any subject ... was extremely tough.
@@ -124,73 +223,6 @@ const Home = (props) => {
             </cite>
           </footer>
         </BlockQuote>
-        <Title textAlign="center" margin="35px">
-          Jump into the global mainstream. Learn English while you learn to
-          code.
-        </Title>
-        <RegisterButton />
-      </ContentSection>
-      <Title textAlign="center">
-        Build Technical and Conversational Fluency
-      </Title>
-      <ContentSectionLanding>
-        <p>
-          You practice conversation in every class, specifically on software
-          engineering.
-        </p>
-        <p>
-          Topics range from working with product managers, co-workers, clients,
-          performing code reviews, asking questions on forums, reading
-          documentation, and many more.
-        </p>
-        <I>And build real software in the process.</I>
-        <RegisterButton />
-      </ContentSectionLanding>
-      <Title textAlign="center">Create Your Technical Portfolio</Title>
-      <ContentSectionLanding>
-        <p>Build your portfolio as you go.</p>
-        <p>
-          All your work in classes is yours, for display in your portfolio
-          WebApp. Show-off to potential employers your skills in ReactJS,
-          Python, and MySQL, with professional code in a GitHub repository, and
-          a real, deployed WebApp.
-        </p>
-        <ImgContainer>
-          {[
-            "react-logo-150.png",
-            "python-logo-150.png",
-            "mysql-logo-150.png",
-          ].map((src, idx) => (
-            <img
-              alt={src}
-              key={idx}
-              style={{ height: "150px" }}
-              src={`${props.settings.assets}/${src}`}
-            />
-          ))}
-        </ImgContainer>
-        <RegisterButton />
-      </ContentSectionLanding>
-      <Title textAlign="center">
-        Learn Vocabulary and Grammar in Real Context
-      </Title>
-      <ContentSectionLanding>
-        <p>
-          We supplement all courses with grammar and vocabulary practices,
-          tightly integrated into technical lessons.
-        </p>
-        <RegisterButton />
-      </ContentSectionLanding>
-      <Title textAlign="center">Learn in a Live Teaching Environment</Title>
-      <ContentSectionLanding>
-        <p>
-          With so many classes moving to massive open online courses, students
-          are left to learn in isolation. We bring back the warmth, motivation,
-          and community that online classes are currently lacking. Do pair work
-          activities in class, ask your instructor questions, laugh, and share
-          your doubts and your accomplishments!
-        </p>
-        <RegisterButton />
       </ContentSectionLanding>
     </MainLanding>
   );
