2f5ee68931e7ff7befb24656434b589dc82cd381
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-03-25 16:06:21 -0400

    fixed missing email in validate-email call, sticky email issue not appearing

--- web/src/CheckoutForm/index.js
+++ web/src/CheckoutForm/index.js
@@ -1,56 +1,72 @@
-import axios from 'axios';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
-import React, { useState } from 'react';
+import axios from "axios";
+import { computed, observable } from "mobx";
+import { observer } from "mobx-react";
+import React, { useState } from "react";
 
 import {
-  Elements, CardElement, useStripe, useElements
-} from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-import settings from '../settings';
-import { cieApi } from '../services/cieApi';
-import { AlertMessage, Button, Spinner } from '../UtilComponents';
-import { CcySelect, EmailNote, Form, PaymentInfo, PmtFormLabel, NameField, BuyButton } from './subcomponents';
-
+import settings from "../settings";
+import { cieApi } from "../services/cieApi";
+import { AlertMessage, Button, Spinner } from "../UtilComponents";
+import {
+  CcySelect,
+  EmailNote,
+  Form,
+  PaymentInfo,
+  PmtFormLabel,
+  NameField,
+  BuyButton,
+} from "./subcomponents";
 
 const { stripePK } = settings;
 const stripePromise = loadStripe(stripePK);
 
-let paymentCurrency = observable.box('EUR');
-const setPaymentCurrency = (e) => paymentCurrency = e.target.value;
+let paymentCurrency = observable.box("EUR");
+const setPaymentCurrency = (e) => (paymentCurrency = e.target.value);
 const CurrencyMenu = () => {
   return (
     <CcySelect value={paymentCurrency} onChange={setPaymentCurrency}>
-      {['EUR', 'USD'].map((currency, idx) => <option key={idx} value={currency}>{currency}</option>)}
+      {["EUR", "USD"].map((currency, idx) => (
+        <option key={idx} value={currency}>
+          {currency}
+        </option>
+      ))}
     </CcySelect>
-  )
+  );
 };
 
 function CheckoutFormConsumer(props) {
   const { appStore, onCloseClick, sessionData } = props;
   const stripe = useStripe();
   const elements = useElements();
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [email, setEmail] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [isComplete, setComplete] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
-  const [isInvalidEmail, setIsInvalidEmail] = useState('');
-
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isInvalidEmail, setIsInvalidEmail] = useState("");
+  console.log("Email in appStore:", appStore.email);
   const computedEmail = email || appStore.email; // prioritize a manually entered email over the email we find in the auth user object.
 
-  const handleSubmit = async (event) => {
+  const handlePurchaseProcess = async (computedEmail) => {
+    console.log("handlePurchaseProcess() computedEmail:", computedEmail);
     // disable default form submission
-    event.preventDefault();
+    // event.preventDefault();
 
     /**
      * Calls API backend to validate email. If email is invalid, returns true.
-     * @param {string} email 
+     * @param {string} email
      */
     async function emailValidationFailure(email) {
+      console.log("Validating email", email);
       try {
-        const res = await axios.put('/api/payment/validate-email', { email });
+        const res = await axios.put("/api/payment/validate-email", { email });
         const { errors } = res.data;
         const hasErrors = errors !== undefined && errors.length > 0;
         if (hasErrors) {
@@ -62,7 +78,9 @@ function CheckoutFormConsumer(props) {
         // the http call to the backend must have failed.
         setLoading(false);
         console.log("Error calling email validation on the backend:", error);
-        setErrorMsg("An internal error ocurred. You might want to try again. The issue has been logged for investigation.");
+        setErrorMsg(
+          "An internal error ocurred. You might want to try again. The issue has been logged for investigation."
+        );
         return true;
       }
       return false;
@@ -74,17 +92,25 @@ function CheckoutFormConsumer(props) {
     async function createPaymentIntent(intentParams) {
       let clientSecret;
       try {
-        const resp = await axios.post('/api/payment/create-payment-intent', intentParams);
+        const resp = await axios.post(
+          "/api/payment/create-payment-intent",
+          intentParams
+        );
         clientSecret = resp.data.clientSecret;
       } catch (error) {
-        setErrorMsg("We couldn't process your order. You have not been charged. The issue has been logged for investigation.");
+        setErrorMsg(
+          "We couldn't process your order. You have not been charged. The issue has been logged for investigation."
+        );
         setLoading(false);
         console.log(error);
         try {
           const failReason = { ...error, email: computedEmail };
-          await axios.post('/api/payment/failure', failReason);
+          await axios.post("/api/payment/failure", failReason);
         } catch (err2) {
-          console.log("Failed to capture reason for create-payment-intent failure. Epic!", err2)
+          console.log(
+            "Failed to capture reason for create-payment-intent failure. Epic!",
+            err2
+          );
         }
         return null;
       }
@@ -97,9 +123,11 @@ function CheckoutFormConsumer(props) {
       setComplete(true);
     }
 
-    const noEmail = email === null || email === '';
+    const noEmail = email === null || email === "";
     if (noEmail) {
-      setIsInvalidEmail("Hey! We need your email address to send you a receipt and class information. Please enter one. :)")
+      setIsInvalidEmail(
+        "Hey! We need your email address to send you a receipt and class information. Please enter one. :)"
+      );
     }
 
     const stripeNotLoaded = !stripe || !elements;
@@ -109,7 +137,7 @@ function CheckoutFormConsumer(props) {
 
     setLoading(true);
 
-    if (await emailValidationFailure(email)) {
+    if (await emailValidationFailure(computedEmail)) {
       // if validation fails, exit early.
       setLoading(false);
       return;
@@ -118,7 +146,7 @@ function CheckoutFormConsumer(props) {
     const intentParams = {
       item: sessionData.id,
       currency: paymentCurrency,
-      email: email
+      email: email,
     };
 
     const clientSecret = await createPaymentIntent(intentParams);
@@ -130,90 +158,119 @@ function CheckoutFormConsumer(props) {
           name,
           email,
         },
-      }
+      },
     };
 
     let confirmationResp;
-    const confirmationFailure = "Something went wrong when we tried to send your confirmation email, but your class was purchased successfully. We will reach out to you shortly.";
+    const confirmationFailure =
+      "Something went wrong when we tried to send your confirmation email, but your class was purchased successfully. We will reach out to you shortly.";
     const result = await stripe.confirmCardPayment(clientSecret, paymentMethod);
     if (result.error) {
       console.log(result);
-      await axios.post('/api/payment/failure', result.error);
+      await axios.post("/api/payment/failure", result.error);
       setErrorMsg(result.error.message);
       setLoading(false);
     } else {
       // The payment was processed.
       console.log(result);
-      if (result.paymentIntent.status === 'succeeded') {
+      if (result.paymentIntent.status === "succeeded") {
         setComplete(true);
         setLoading(false);
-        confirmationResp = cieApi.sendPaymentConfirmation({
-          email: computedEmail,
-          /**
-           * intentionally stick with login user's name if different from card name (don't use a computedName),
-           * this is in case the user uses a card that isn't their own.
-           *  */
-          name: name,
-          moduleSessionId: sessionData.id,
-          isAuthenticated: appStore.authData !== null,
-          paymentResult: result
-        }).catch(err => {
-          console.log("Failed to send payment confirmation:", err); errorAndSetComplete(confirmationFailure);
-        })
+        confirmationResp = cieApi
+          .sendPaymentConfirmation({
+            email: computedEmail,
+            /**
+             * intentionally stick with login user's name if different from card name (don't use a computedName),
+             * this is in case the user uses a card that isn't their own.
+             *  */
+            name: name,
+            moduleSessionId: sessionData.id,
+            isAuthenticated: appStore.authData !== null,
+            paymentResult: result,
+          })
+          .catch((err) => {
+            console.log("Failed to send payment confirmation:", err);
+            errorAndSetComplete(confirmationFailure);
+          });
       }
-
     }
-    if (confirmationResp === 'undefined' || confirmationResp.success === false) {
+    if (
+      confirmationResp === "undefined" ||
+      confirmationResp.success === false
+    ) {
       errorAndSetComplete(confirmationFailure);
     }
-  }
-
+  };
 
   return (
     <>
-      {isComplete ?
+      {isComplete ? (
         <>
-          <p>Your purchase is complete! Check your email for further instructions. If you do not see the confirmation email, please check your spam folder. See you in class!</p>
-          {errorMsg && <AlertMessage style={{ marginTop: '3px' }} text={errorMsg} />}
+          <p>
+            Your purchase is complete! Check your email for further
+            instructions. If you do not see the confirmation email, please check
+            your spam folder. See you in class!
+          </p>
+          {errorMsg && (
+            <AlertMessage style={{ marginTop: "3px" }} text={errorMsg} />
+          )}
           <Button onClick={onCloseClick}>CLOSE</Button>
         </>
-        :
-        <Form onSubmit={handleSubmit}>
+      ) : (
+        <Form onSubmit={() => handlePurchaseProcess(computedEmail)}>
           <PaymentInfo className="FormGroup">
             <PmtFormLabel>Name</PmtFormLabel>
-            <NameField placeholder="Your name here" value={name} onChange={e => setName(e.target.value)} />
-            <PmtFormLabel>Card Details</PmtFormLabel><CardElement />
+            <NameField
+              placeholder="Your name here"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <PmtFormLabel>Card Details</PmtFormLabel>
+            <CardElement />
             {/* <PmtFormLabel>Postal/Zip Code</PmtFormLabel><NameField placeholder="08000" value={postalCode} onChange={e => setPostalCode(e.target.value)} /> */}
             <PmtFormLabel>Currency</PmtFormLabel>
             <CurrencyMenu />
 
             <PmtFormLabel>Email</PmtFormLabel>
             <div>
-              <NameField placeholder="name@domain.com"
-                value={computedEmail || ''} // need this empty string for React to "control" this input field
-                onChange={e => setEmail(e.target.value)}
+              <NameField
+                placeholder="name@domain.com"
+                value={computedEmail || ""} // need this empty string for React to "control" this input field
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <EmailNote>For receipt and account creation on your terms. We will NOT spam you.</EmailNote>
+              <EmailNote>
+                For receipt and account creation on your terms. We will NOT spam
+                you.
+              </EmailNote>
             </div>
 
-            <BuyButton onClick={handleSubmit} disabled={!stripe || isLoading}>
+            <BuyButton
+              onClick={() => handlePurchaseProcess(computedEmail)}
+              disabled={!stripe || isLoading}
+            >
               PURCHASE
             </BuyButton>
           </PaymentInfo>
           {isLoading && <Spinner />}
-          {(!isLoading && errorMsg) && <AlertMessage style={{ marginTop: '3px' }} text={errorMsg} />}
-        </Form>}
+          {!isLoading && errorMsg && (
+            <AlertMessage style={{ marginTop: "3px" }} text={errorMsg} />
+          )}
+        </Form>
+      )}
     </>
   );
 }
 
-
 function CheckoutForm(props) {
   return (
     <Elements stripe={stripePromise}>
-      <CheckoutFormConsumer appStore={props.appStore} onCloseClick={props.onCloseClick} sessionData={props.sessionData} />
+      <CheckoutFormConsumer
+        appStore={props.appStore}
+        onCloseClick={props.onCloseClick}
+        sessionData={props.sessionData}
+      />
     </Elements>
-  )
+  );
 }
 
 const CheckoutFormObserver = observer(CheckoutForm);
