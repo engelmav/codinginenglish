cab09b4f41747e4655cc0a132df73abae8bf3fbb
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-05-16 14:51:35 -0400

    designer

--- web/src/ExerciseDesigner/DesignCanvas.js
+++ web/src/ExerciseDesigner/DesignCanvas.js
@@ -3,6 +3,17 @@ import PropTypes from "prop-types";
 import fabric from "fabric";
 import Dialog from "@material-ui/core/Dialog";
 import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+import { SketchPicker } from "react-color";
+
+const Toolbar = styled.div`
+  flex-direction: row;
+`;
+const CanvasContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 class DesignCanvas extends React.Component {
   static propTypes = {
@@ -11,8 +22,8 @@ class DesignCanvas extends React.Component {
   };
 
   static defaultProps = {
-    width: 600,
-    height: 400,
+    width: 800,
+    height: 600,
   };
 
   state = {
@@ -46,12 +57,36 @@ class DesignCanvas extends React.Component {
   applyGenericAttrs = (obj, uuid) => {
     obj.set({
       onDeselect: () => this.setState({ selectedObj: null }),
-      onSelect: (e) => {
+      onSelect: () => {
         this.setSelectedObject(uuid);
       },
     });
     return obj;
-  }
+  };
+
+  handleColorChange = (color, event) => {
+    const obj = this.state.canvas.getActiveObject();
+    obj.set({ fill: color.hex });
+    this.state.canvas.renderAll();
+  };
+
+  setCanvasJson = (event) => {
+    const json = event.target.value;
+    console.log("event received in setCanvas:", event.target.value)
+    const loadAndRender = () => {
+      this.state.canvas.loadFromJSON(
+        json,
+        () => {
+          console.log("re-rendering canvas")
+          this.state.canvas.renderAll();
+        },
+        function (o, object) {
+          console.log(o, object);
+        }
+      );
+    };
+    this.setState({ canvasJson: json }, loadAndRender);
+  };
 
   render() {
     const { width, height } = this.props;
@@ -63,132 +98,191 @@ class DesignCanvas extends React.Component {
       : false;
     return (
       <>
-        <canvas ref={(c) => (this.c = c)} width={width} height={height} />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(JSON.stringify(this.state.canvas.toJSON()));
-          }}
-        >
-          To JSON
-        </button>
-        <button
-          onClick={() => {
-            const uuid = uuidv4();
-            const text = new fabric.fabric.IText("Click to edit", {
-              id: uuid,
-              fontFamily: "arial black",
-              fontSize: 16,
-              left: 100,
-              top: 100,
-            });
-            const textNormal = this.applyGenericAttrs(text, uuid);
-            const obj = {
-              uuid: uuid,
-              lockMovementX: true,
-              lockMovementY: true,
-            };
-            this.setState({ objects: this.state.objects.concat(obj) });
-            this.state.canvas.add(textNormal);
-            this.state.canvas.setActiveObject(textNormal);
-          }}
-        >
-          Text
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ dialogOpen: true, temp: "" });
-          }}
-        >
-          Image
-        </button>
-        <button
-          onClick={() => {
-            const obj = this.state.canvas.getActiveObject();
-            const newObjList = this.state.objects.filter(
-              (fobj) => fobj.uuid !== obj.id
-            );
-            this.setState({ objects: newObjList });
-            this.state.canvas.remove(obj);
-            this.state.canvas.renderAll(); //Re-render the drawing in Fabric
-          }}
-        >
-          Delete
-        </button>
+        <CanvasContainer>
+          <canvas
+            style={{ border: "1px solid black", display: "flex" }}
+            ref={(c) => (this.c = c)}
+            width={width}
+            height={height}
+          />
+          <Toolbar>
+            <SketchPicker onChange={this.handleColorChange} />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                console.log(JSON.stringify(this.state.canvas.toJSON()));
+              }}
+            >
+              To JSON
+            </button>
+            <button
+              onClick={() => {
+                const uuid = uuidv4();
+                const text = new fabric.fabric.IText("Click to edit", {
+                  id: uuid,
+                  fontFamily: "arial black",
+                  fontSize: 16,
+                  left: 100,
+                  top: 100,
+                });
+                const textNormal = this.applyGenericAttrs(text, uuid);
+                const obj = {
+                  uuid: uuid,
+                  lockMovementX: true,
+                  lockMovementY: true,
+                };
+                this.setState({ objects: this.state.objects.concat(obj) });
+                this.state.canvas.add(textNormal);
+                this.state.canvas.setActiveObject(textNormal);
+              }}
+            >
+              Text
+            </button>
+            <button
+              onClick={() => {
+                this.setState({ dialogOpen: true, temp: "" });
+              }}
+            >
+              Image
+            </button>
+            <button
+              onClick={() => {
+                const uuid = uuidv4();
+                const rect = new fabric.fabric.Rect({
+                  id: uuid,
+                  left: 100,
+                  top: 100,
+                  width: 50,
+                  height: 50,
+                });
+                const rectNormal = this.applyGenericAttrs(rect, uuid);
+                const obj = {
+                  uuid: uuid,
+                  lockMovementX: true,
+                  lockMovementY: true,
+                };
+                this.setState({ objects: this.state.objects.concat(obj) });
+                this.state.canvas.add(rectNormal);
+                this.state.canvas.setActiveObject(rectNormal);
+              }}
+            >
+              Rectangle
+            </button>
+            <button
+              onClick={() => {
+                const uuid = uuidv4();
+                const rect = new fabric.fabric.Circle({
+                  id: uuid,
+                  left: 100,
+                  top: 100,
+                  radius: 1,
+                  fill: "black",
+                });
+                const rectNormal = this.applyGenericAttrs(rect, uuid);
+                const obj = {
+                  uuid: uuid,
+                  lockMovementX: true,
+                  lockMovementY: true,
+                };
+                this.setState({ objects: this.state.objects.concat(obj) });
+                this.state.canvas.add(rectNormal);
+                this.state.canvas.setActiveObject(rectNormal);
+              }}
+            >
+              Circle
+            </button>
+            <button
+              onClick={() => {
+                const obj = this.state.canvas.getActiveObject();
+                const newObjList = this.state.objects.filter(
+                  (fobj) => fobj.uuid !== obj.id
+                );
+                this.setState({ objects: newObjList });
+                this.state.canvas.remove(obj);
+                this.state.canvas.renderAll(); //Re-render the drawing in Fabric
+              }}
+            >
+              Delete
+            </button>
 
-        <label>student can:</label>
-        {this.state.selectedObj && (
-          <label>
-            {this.state.canvas && (
-              <input
-                key={this.state.selectedObj}
-                name="selectable"
-                type="checkbox"
-                checked={isMovable}
-                onChange={(e) => {
-                  const obj = this.state.canvas.getActiveObject();
-                  console.log("got selected object", obj);
-                  // obj.hasControls = false;
-                  // obj.hasBorders = false;
-                  // obj.editable = false;
-                  // this.state.canvas.selection = selectable;
-                  this.setState({
-                    objects: this.state.objects.map((obj) => {
-                      if (selectedObj.uuid === obj.uuid) {
-                        return {
-                          ...obj,
-                          lockMovementX: !obj.lockMovementX,
-                          lockMovementY: !obj.lockMovementY,
-                        };
-                      }
-                      return obj;
-                    }),
-                  });
+            <label>student can:</label>
+            {this.state.selectedObj && (
+              <label>
+                {this.state.canvas && (
+                  <input
+                    key={this.state.selectedObj}
+                    name="selectable"
+                    type="checkbox"
+                    checked={isMovable}
+                    onChange={(e) => {
+                      const obj = this.state.canvas.getActiveObject();
+                      console.log("got selected object", obj);
+                      // obj.hasControls = false;
+                      // obj.hasBorders = false;
+                      // obj.editable = false;
+                      // this.state.canvas.selection = selectable;
+                      this.setState({
+                        objects: this.state.objects.map((obj) => {
+                          if (selectedObj.uuid === obj.uuid) {
+                            return {
+                              ...obj,
+                              lockMovementX: !obj.lockMovementX,
+                              lockMovementY: !obj.lockMovementY,
+                            };
+                          }
+                          return obj;
+                        }),
+                      });
 
-                  this.state.canvas.renderAll();
-                }}
-              />
+                      this.state.canvas.renderAll();
+                    }}
+                  />
+                )}
+                Move
+              </label>
             )}
-            Move
-          </label>
-        )}
-        <Dialog open={this.state.dialogOpen}>
-          <input
-            type="text"
-            placeholder="image url"
-            onChange={(e) => this.setState({ temp: e.target.value })}
-          ></input>
-          <button
-            onClick={() => {
-              const uuid = uuidv4();
-              const image = fabric.fabric.Image.fromURL(
-                this.state.temp,
-                (img) => {
-                  //i create an extra var for to change some image properties
-                  var img1 = img.set({
-                    id: uuid,
-                    left: 0,
-                    top: 0,
-                    // width: 150,
-                    // height: 150,
-                  });
-                  const imgNormal = this.applyGenericAttrs(img1, uuid);
-                  const obj = {
-                    uuid,
-                    lockMovementX: false,
-                    lockMovementY: false,
-                  };
-                  this.setState({ objects: this.state.objects.concat(obj) });
-                  this.state.canvas.add(imgNormal);
-                }
-              );
+          </Toolbar>
+          <Dialog open={this.state.dialogOpen}>
+            <input
+              type="text"
+              placeholder="image url"
+              onChange={(e) => this.setState({ temp: e.target.value })}
+            ></input>
+            <button
+              onClick={() => {
+                const uuid = uuidv4();
+                const image = fabric.fabric.Image.fromURL(
+                  this.state.temp,
+                  (img) => {
+                    //i create an extra var for to change some image properties
+                    var img1 = img.set({
+                      id: uuid,
+                      left: 0,
+                      top: 0,
+                      // width: 150,
+                      // height: 150,
+                    });
+                    const imgNormal = this.applyGenericAttrs(img1, uuid);
+                    const obj = {
+                      uuid,
+                      lockMovementX: false,
+                      lockMovementY: false,
+                    };
+                    this.setState({ objects: this.state.objects.concat(obj) });
+                    this.state.canvas.add(imgNormal);
+                  }
+                );
 
-              this.setState({ dialogOpen: false, temp: "" });
-            }}
-          >
-            Add
-          </button>
-        </Dialog>
+                this.setState({ dialogOpen: false, temp: "" });
+              }}
+            >
+              Add
+            </button>
+          </Dialog>
+          <textarea onChange={this.setCanvasJson} value={this.state.canvasJson}>
+
+          </textarea>
+        </CanvasContainer>
       </>
     );
   }
