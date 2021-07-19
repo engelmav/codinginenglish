074d5422039aef05842acfcf3b6f2440ca6cd94c
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:24:17 -0400

    inject history into App props

--- web/src/App/index.js
+++ web/src/App/index.js
@@ -2,11 +2,9 @@ import React, { Component } from "react";
 import "./styles.css";
 import { Router } from "react-router-dom";
 import styled from "styled-components";
-import history from "../history";
 import { observer } from "mobx-react";
 import { Flex } from "rebass";
 import { Box } from "../UtilComponents/Box";
-import ReactGA from "react-ga";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { color, backgroundColor } from "styled-system";
 
@@ -24,14 +22,6 @@ const theme = {
   },
 };
 
-const trackingId = "UA-199972795-1";
-ReactGA.initialize(trackingId);
-
-history.listen((location) => {
-  ReactGA.pageview(location.pathname);
-});
-
-history.listen(() => window.scrollTo(0, 0));
 
 const RoutesBox = styled(Box)`
   flex: 1;
@@ -44,7 +34,7 @@ const WordBtn = styled.button`
 @observer
 class App extends Component {
   render() {
-    const { Header, Routes, Footer } = this.props;
+    const { Header, Routes, Footer, history } = this.props;
     return (
       <ThemeProvider theme={theme}>
         <Router history={history}>
