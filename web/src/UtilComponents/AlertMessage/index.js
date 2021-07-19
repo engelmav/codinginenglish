e3014a7fbd1c97d0cb3317438134235711c683b4
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:29:21 -0400

    convert to styled-component

--- web/src/UtilComponents/AlertMessage/index.js
+++ web/src/UtilComponents/AlertMessage/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import styles from './style.module.css';
+import React from "react";
+import styled from "styled-components";
+import { Box } from "../Box";
+import { typography } from "styled-system";
+import { fontFamily } from "../sharedStyles";
+
+const StyledDiv = styled(Box)`
+  border: 1px solid red;
+  border-radius: 4px;
+  color: red;
+  ${typography}
+  font-family: ${fontFamily};
+`;
 
 const AlertMessage = (props) => {
-    const { style, text } = props;
-    return (
-        <div className={styles.border} style={style}>{text}</div>
-    );
-}
+  const { text } = props;
+  return <StyledDiv {...props}>{text}</StyledDiv>;
+};
 
 export { AlertMessage };
