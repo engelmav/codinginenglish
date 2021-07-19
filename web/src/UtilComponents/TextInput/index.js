fd4c95585099e9f8ff4ce327c68c55b13dc53357
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:31:14 -0400

    ClearableTextInput

--- web/src/UtilComponents/TextInput/index.js
+++ web/src/UtilComponents/TextInput/index.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { space } from "styled-system";
 import {
+  darkGray,
   fontFamily,
   lgInputFontSize,
   smInputFontSize,
   whenSmallScreen,
 } from "../sharedStyles";
+import { boxy } from "../Box";
+import React, { useEffect } from "react";
+import { FaTimesCircle } from "@react-icons/all-files/fa/FaTimesCircle";
 
 export const inputFormBorder = "1px dotted black";
 export const inputFormBorderRadius = "3px";
@@ -26,3 +30,36 @@ TextInput.defaultProps = {
   p: 2,
 };
 export { TextInput };
+
+const ButtonDiv = styled.div`
+  ${boxy}
+  position: relative;
+  input {
+    width: 100%;
+  }
+  div {
+    position: absolute;
+
+    right: 11px;
+    z-index: 2;
+    border: none;
+    top: 10px;
+    height: 30px;
+    cursor: pointer;
+    color: ${darkGray};
+
+    transform: translateX(2px);
+  }
+`;
+export const ClearableTextInput = (props) => {
+  useEffect(() => {}, [props.value]);
+  const { id, name, onChange, onClear, value } = props;
+  return (
+    <ButtonDiv>
+      <TextInput id={id} name={name} value={value || ""} {...props} onChange={onChange} />
+      <div onClick={onClear}>
+        <FaTimesCircle />
+      </div>
+    </ButtonDiv>
+  );
+};
