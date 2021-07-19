18c912e4e9fcb12bc2cea0f4f2136e34a959005a
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:30:22 -0400

    button default padding, commonStyles refactor

--- web/src/UtilComponents/Button/index.js
+++ web/src/UtilComponents/Button/index.js
@@ -1,30 +1,56 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { space, flexbox } from "styled-system";
 import { font, cieOrange } from "../sharedStyles";
 
-const Button = styled.button`
+const commonStyles = css`
   ${font}
-  background-color: ${cieOrange};
-  border: solid #ff3e00;
-  border-width: 1px;
-  color: white;
-  padding: 8px;
-  border-radius: 2px;
-  cursor: pointer;
   ${space}
   ${flexbox}
+  background-color: ${cieOrange};
+  border-radius: 2px;
+  color: white;
+  cursor: pointer;
+`;
+
+const hoverStyles = css`
+  color: rgba(255, 255, 255, 1);
+  box-shadow: 0 5px 15px rgba(145, 92, 182, 0.4);
+`;
+
+const Button = styled.button.attrs((props) => {
+  return { ...props };
+})`
+  ${space}
+  ${commonStyles}
+  border: solid #ff3e00;
+  border-width: 1px;
   &:disabled,
-  &[disabled]{
+  &[disabled] {
     border: 1px solid #999999;
     background-color: #cccccc;
     color: #666666;
-    &:hover {  };
-  };
-  &:hover:enabled { 
-    color: rgba(255, 255, 255, 1);
-    box-shadow: 0 5px 15px rgba(145, 92, 182, .4);
-  };
+    &:hover {
+    }
+  }
+  &:hover:enabled {
+    ${hoverStyles}
+  }
+`;
+Button.defaultProps = {
+  p: 2,
+};
+
+export const LinkButton = styled.a`
+  ${commonStyles}
+  ${font}
+  text-decoration: none;
+  display: inline-block;
+  text-align: center;
+  padding: 10px;
+
+  &:hover {
+    ${hoverStyles}
+  }
 `;
 
-/** @component */
 export { Button };
