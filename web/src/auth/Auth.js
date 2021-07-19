07a63ed94fb892f49a1b607883e3b3c2801e3b99
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:32:21 -0400

    login with email method

--- web/src/auth/Auth.js
+++ web/src/auth/Auth.js
@@ -49,12 +49,12 @@ class Auth {
       this.auth0.parseHash((err, authResult) => {
         if (err) return reject(err);
         if (err || !authResult || !authResult.idToken) {
-          console.log("There was an issue attempting to login.")
+          console.log("There was an issue attempting to login.");
         }
         this.setSession(authResult);
         this.onAuthSuccess.forEach((callback) => callback(authResult));
         resolve();
-      }); 
+      });
     });
   }
 
@@ -126,32 +126,36 @@ class Auth {
       }
     });
   }
-  loginWithGoogle(cb = null){
-    this.auth0.authorize({
-      connection: "google-oauth2",
-    }, (err, res) => {
-      if (cb) cb(err, res);
-    })
-  }
-  loginWithEmailLink(email, cb = null){
-    return new Promise((resolve, reject) => {
-      
-      this.auth0.passwordlessStart({
-        email,
-        send: "link",
-        connection: "email"
-      }, (err, res) => {
+  loginWithGoogle(cb = null) {
+    this.auth0.authorize(
+      {
+        connection: "google-oauth2",
+      },
+      (err, res) => {
         if (cb) cb(err, res);
-        if (err){
-          console.log("error ocurred in passwordless login:", err);
+      }
+    );
+  }
+  loginWithEmailLink(email, cb = null) {
+    return new Promise((resolve, reject) => {
+      this.auth0.passwordlessStart(
+        {
+          email,
+          send: "link",
+          connection: "email",
+        },
+        (err, res) => {
+          if (cb) cb(err, res);
+          if (err) {
+            console.log("error ocurred in passwordless login:", err);
+          }
+          if (res) {
+            console.log("successful passwordlessStart:", res);
+            resolve(res);
+          }
         }
-        if (res){
-          console.log("successful passwordlessStart:", res)
-          resolve(res)
-        }
-      })
-    })
-    
+      );
+    });
   }
 }
 
