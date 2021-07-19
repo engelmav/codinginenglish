48ad623f9a4dd0390c86cf132548061b2cb0126d
Vincent Engelmann <vincent.engelmann1@gmail.com>
2021-07-19 12:31:02 -0400

    change default Spinner

--- web/src/UtilComponents/Spinner/index.js
+++ web/src/UtilComponents/Spinner/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import PulseLoader from 'react-spinners/PulseLoader';
+import React from "react";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import styled from "styled-components";
+import { color } from "styled-system";
 
+const StyledScaleLoader = styled(ScaleLoader)`
+  ${color}
+`;
 
-const Spinner = () =>
-        <PulseLoader loading={true} />
-
-
-/** @component */
-export { Spinner };
+export const Spinner = (props) => {
+  const { size } = props;
+  const computedSize = size ? size : 10;
+  return <StyledScaleLoader height={computedSize} loading={true} {...props} />;
+};
