333f19c7b93a0ad20628cc32b2f1e1a8be9d2583
Vincent Engelmann <vincent.engelmann1@gmail.com>
2023-07-16 22:02:04 -0400

    google form nj signup

---
+++ web2/pages/nj-signup.js
@@ -0,0 +1,12 @@
+export default function IndexPage() {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSejl2UYktSuZORW31O8JYdkuz1qle_2b_t8wRgVuLJTJhUQaA/viewform?embedded=true"
+      width="640"
+      height="1336"
+      frameborder="0"
+    >
+      Cargando...
+    </iframe>
+  );
+}
