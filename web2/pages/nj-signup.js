2dc58875550e259a058a557f781876b4251a6443
Vincent Engelmann <vincent.engelmann1@gmail.com>
2023-07-18 22:48:35 -0400

    embed default height

--- web2/pages/nj-signup.js
+++ web2/pages/nj-signup.js
@@ -1,12 +1,33 @@
+import styled from "@emotion/styled";
+import { ContentSection } from "../components/Layout";
+import { H1, H2 as H2Base, P } from "../components/typography";
+
+const H2 = styled(H2Base)`
+  margin-top: 1em;
+`;
+
 export default function IndexPage() {
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSejl2UYktSuZORW31O8JYdkuz1qle_2b_t8wRgVuLJTJhUQaA/viewform?embedded=true"
-      width="640"
-      height="1336"
-      frameborder="0"
-    >
-      Cargando...
-    </iframe>
+    <>
+      <ContentSection>
+        <H1>Coding in English (remoto)</H1>
+        <H2>25 septiembre - 6:30pm - 8:30pm - $25 por sesión</H2>
+      </ContentSection>
+      <div
+        style={{
+          width: "100%",
+          height: "1700px",
+          overflow: "hidden",
+        }}
+      >
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSejl2UYktSuZORW31O8JYdkuz1qle_2b_t8wRgVuLJTJhUQaA/viewform?embedded=true"
+          frameborder="0"
+          style={{ width: "100%", height: "calc(100% - 3px)" }}
+        >
+          Cargando...
+        </iframe>
+      </div>
+    </>
   );
 }
